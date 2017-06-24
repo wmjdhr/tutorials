57359b6aafeb3887a3f68c7e69e422217018c7a4
Mike <wmjdhr@sina.com>
2017-06-25 00:13:43 +0800

    first lesson

---
+++ decorator.py
@@ -0,0 +1,16 @@
+from functools import wraps
+import time
+
+
+def timethis(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end - start)
+        return result
+
+    return wrapper
+
